3c5d23d2098ace9fcaee909da78d26dc8ba50a02
Atharv Ajetrao <180567959+Binary-Bard-sudo@users.noreply.github.com>
2025-06-13 20:33:03 +0530

    Add files via upload

---
+++ motor on off.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=motor_on_off.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/motor_on_off.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=motoronoff.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/motoronoff.x.tar
